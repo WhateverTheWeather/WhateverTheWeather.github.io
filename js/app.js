8cdd6e72da994540053bd6bde4aa1e5b184f03ca
Kieran <rave@kierans-mbp.client.rave.ac.uk>
2015-11-26 12:03:42 +0000

    its all looking good. notes are included weather api key added

---
+++ js/app.js
@@ -0,0 +1,94 @@
+Parse.initialize("wDjVVlo0q9ZJzzFOsfkNKsFIf3xWqwLXwXv29T2h", "qfWAIOVo9qEGQG2R6K0k9OnbLDApmLktMyjZ6KY5")
+
+
+/*
+
+1. Get geolocation and store user position in a variable
+2. Use user position to search for the weather
+3. Using the user position and the weather type, ask Parse for places appropriate to the weather within XXkm from user position 
+4. Display on a map
+
+*/
+
+
+/*var Person = Parse.Object.extend("Person")
+      
+var kieran = new Person()
+
+kieran.set('name', 'kieran')
+kieran.set('year', 1)*/
+
+navigator.geolocation.watchPosition(gotPosition)
+
+function gotPosition(position) 
+{
+    
+    
+    getTheWeather(position)
+    
+   /*console.log( "Latitude: " + position.coords.latitude + "Longitude: " + position.coords.longitude);*/
+   
+    /*var point = new Parse.GeoPoint(
+    {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+    });
+
+    kieran.set('position', point)
+
+    kieran.save(null,
+    {
+        success: function(object)
+        {
+        // console.log(object)
+        console.log('My latitude is: ' + position.coords.latitude);
+        console.log('My Longitude is: ' + position.coords.longitude);
+        },
+        error: function(object,error)
+        {
+        console.error(error)
+        }
+    })*/
+}
+
+// 2
+
+var APIKey = '3a258cbc6780350f0f0862c9708f21a5'
+
+function getTheWeather(position)
+{
+    var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&APPID=' + APIKey
+    console.log(url)
+    // do the jQuery AJAX call (see Instagram exercise)
+    
+    // 
+}
+
+
+// 3 
+
+var currentWeather = 'Foggy' // hardcoded for now
+
+var Place = Parse.Object.extend('places')
+
+var query = new Parse.Query(Place)
+query.equalTo(currentWeather, true)
+query.find(
+{
+    success: function(results) 
+    {
+        console.log("Successfully retrieved " + results.length + " places.")
+
+        // Do something with the returned Parse.Object values
+        for (var i = 0; i < results.length; i++) 
+        {
+            var place = results[i]
+            console.log(place.get('Name'))
+        }
+
+    },
+    error: function(error) {
+        console.error("Error: " + error.code + " " + error.message)
+    }
+
+})
